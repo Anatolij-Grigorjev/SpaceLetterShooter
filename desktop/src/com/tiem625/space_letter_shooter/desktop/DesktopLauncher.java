d120127f2278ecc3134f379fbe5d07a66629eb4c
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2020-10-02 18:41:26 +0300

    initial empty commit with jdk7 configured

---
+++ desktop/src/com/tiem625/space_letter_shooter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tiem625.space_letter_shooter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tiem625.space_letter_shooter.Launcher;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Launcher(), config);
+	}
+}
