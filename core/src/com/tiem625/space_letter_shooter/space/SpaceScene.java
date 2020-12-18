b2e3fb706b29131cbb0151325532a95d7fbb1f7d
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2020-12-18 13:57:17 +0200

    make ships bounce screen downwards after appearing

--- core/src/com/tiem625/space_letter_shooter/space/SpaceScene.java
+++ core/src/com/tiem625/space_letter_shooter/space/SpaceScene.java
@@ -1,11 +1,15 @@
 package com.tiem625.space_letter_shooter.space;
 
 import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.actions.DelayAction;
+import com.tiem625.space_letter_shooter.config.GamePropsHolder;
 import com.tiem625.space_letter_shooter.config.Viewports;
 import com.tiem625.space_letter_shooter.scene.Scene;
 import com.tiem625.space_letter_shooter.space.dto.SceneConfigureSpec;
@@ -38,12 +42,25 @@ public class SpaceScene extends Scene {
                 )
                 .collect(Collectors.toMap(Pair::getValue, Pair::getKey));
 
-        setupShipsFlyToStartActions(shipDesiredPositions);
-        setupShipsFlyDownActions();
+        var totalSetupDelayAction = setupShipsFlyToStartActions(shipDesiredPositions);
+        System.out.println("Total startup delay: " + totalSetupDelayAction.getDuration());
+
+        enemyShips().forEach(ship -> {
+
+            ship.addAction(Actions.sequence(
+                    Actions.delay(totalSetupDelayAction.getDuration() + 0.5f),
+                    Actions.run(() -> ship.addAction(buildShipBounceNearestEdgeAction(ship)))
+            ));
+        });
     }
 
-    private void setupShipsFlyDownActions() {
-
+    private Action buildShipBounceNearestEdgeAction(EnemyShip ship) {
+        var speed = MathUtils.random(150f, 200f);
+        var descentY = MathUtils.random(-10f, -20f);
+        var moveByX = ship.getX() <= GamePropsHolder.props.getResolutionWidth() / 2f ?
+                0 - ship.getX() : GamePropsHolder.props.getResolutionWidth() - ship.getShipTextureSize().x - ship.getX();
+        var distance = new Vector2(moveByX, Math.abs(descentY)).len();
+        return Actions.moveBy(moveByX, descentY, distance / speed, Interpolation.sine);
     }
 
     public EnemyShip addEnemyShip(EnemyShip ship) {
@@ -72,8 +89,8 @@ public class SpaceScene extends Scene {
         return addAndGetStage(new Stage(Viewports.FIT_FULLSCREEN));
     }
 
-    private void setupShipsFlyToStartActions(Map<EnemyShip, Vector2> shipDesiredPositions) {
-        shipDesiredPositions.entrySet().stream()
+    private DelayAction setupShipsFlyToStartActions(Map<EnemyShip, Vector2> shipDesiredPositions) {
+        return shipDesiredPositions.entrySet().stream()
                 .reduce(Actions.delay(0.5f), (prevDelay, shipPositionPair) -> {
                     var ship = shipPositionPair.getKey();
                     var startPosition = shipPositionPair.getValue();
